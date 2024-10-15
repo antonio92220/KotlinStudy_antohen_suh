aca021d53495c09fd4ba2ca6b025321d54baa952
antohen-suh <ant.suxarew2012@yandex.ru>
2024-10-15 23:53:19 +0300

    KS-2-2 fix comments

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,11 +1,16 @@
 package lesson_2
 
 fun main() {
-    val salaryPermanentStaff = 50 * 30000
-    val salaryAllStaff = 50 * 30000 + 30 * 20000
-    val averageSalary = salaryAllStaff / 50 + 30
+    val numberOfPermanentStaff = 50
+    val numberOfInterns = 30
+    val averageSalaryOfPermanentStaff = 30000
+    val averageSalaryOfInterns = 20000
+
+    val salaryPermanentStaff = numberOfPermanentStaff * averageSalaryOfPermanentStaff
+    val salaryAllStaff = salaryPermanentStaff + numberOfInterns * averageSalaryOfInterns
+    val averageSalary = salaryAllStaff / (numberOfInterns + numberOfPermanentStaff)
+
     println("Общая З/П постоянных сотрудников = $salaryPermanentStaff")
     println("Общие расходы по З/П = $salaryAllStaff")
     println("Средня З/П в компании = $averageSalary")
-
 }
