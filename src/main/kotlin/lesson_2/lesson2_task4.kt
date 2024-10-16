0c62a9817ea9fcc5a6134c70e52dea840143d459
antohen-suh <ant.suxarew2012@yandex.ru>
2024-10-17 00:53:24 +0300

    KS-2-4 comment fixes

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,10 +3,12 @@ package org.example.lesson_2
 fun main() {
     val numberOfCrystals = 7
     val numberOfIron = 11
-    val buffValue = 0.2
+    val buffValuePercentage = 20
+    val percent = 100
+    val buffValueFraction = buffValuePercentage / percent
 
-    val crystalsGrowthAfterBuff = numberOfCrystals * buffValue
-    val ironGrowthAfterBuff = numberOfIron * buffValue
+    val crystalsGrowthAfterBuff = numberOfCrystals * buffValueFraction
+    val ironGrowthAfterBuff = numberOfIron * buffValueFraction
 
     println("Количество бонусной кристаллической руды = ${crystalsGrowthAfterBuff.toInt()}")
     println("Количество бонусной железной руды = ${ironGrowthAfterBuff.toInt()}")
