d4e1b9d41946396b466255485fdf3693678e5a18
antohen-suh <ant.suxarew2012@yandex.ru>
2024-10-17 22:31:00 +0300

    KS-3-1 added a greeting phrase depending on the time of day

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,25 +2,15 @@ package org.example.lesson_3
 import java.time.LocalTime
 
 fun main() {
-    /*
-    При входе в приложение высвечивается приветствие. Представим, что мы пишем функцию,
-    которая будет выводить приветствие с именем пользователя на экран (в нашем случае в консоль).
-    Tекст приветствия может меняться в зависимости от времени суток.
-    Так, например, выводится приветствие при входе в приложение Сбербанка.
 
-Приветствие и имя пользователя должны храниться в отдельных переменных.
-Вывести в консоль два приветствия (для дня и для вечера), перезаписывая данные в одну исходную переменную.
-     */
     val userName = "Браток"
     val currentTime = LocalTime.now()
     val morningTimeStart = LocalTime.of(6, 0)
     val eveningTimeStart = LocalTime.of(18, 0)
 
-    if (currentTime.isAfter(morningTimeStart) && currentTime.isBefore(eveningTimeStart))
-    {
+    if (currentTime.isAfter(morningTimeStart) && currentTime.isBefore(eveningTimeStart)) {
         println("Добрый день,$userName")
-    }
-    else {
+    } else {
         println("Добрый вечер,$userName")
     }
 }
