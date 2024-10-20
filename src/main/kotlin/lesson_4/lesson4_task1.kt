2be7b394de083bfe885ba7f3ff8446d4c5160673
antohen-suh <ant.suxarew2012@yandex.ru>
2024-10-21 01:12:16 +0300

    KS-4-1 added display of table reservation availability

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,2 +1,10 @@
 package org.example.lesson_4
 
+fun main() {
+    val reservedTablesToday = 13
+    val reservedTablesTomorrow = 9
+    val totalTables = 13
+
+    println("Доступность столиков на сегодня: ${reservedTablesToday < totalTables}")
+    println("Доступность столиков на сегодня: ${reservedTablesTomorrow < totalTables}")
+}
