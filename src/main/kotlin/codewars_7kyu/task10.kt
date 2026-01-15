c35057440a4d759808fce2cf7a5d22aab0507f64
antohen-suh <ant.suxarew2012@yandex.ru>
2026-01-15 22:55:48 +0300

    Перешел на новый уровень кодворс

--- src/main/kotlin/codewars_7kyu/task10.kt
+++ src/main/kotlin/codewars_7kyu/task10.kt
@@ -1,2 +1,18 @@
 package codewars_7kyu
 
+fun jadenText(cc: String): String {
+    return cc.split(" ").map { it.replaceFirstChar { it.uppercase()} }.joinToString(" ")
+}
+
+
+
+
+/*
+Задача: "Jaden Casing Strings" Джейден Смит (сын Уилла Смита) известен тем, что в Твиттере пишет Каждое Слово
+С Большой Буквы. Твоя задача: превратить обычное предложение в стиль Джейдена.
+
+Пример:
+
+Вход: "How can mirrors be real if our eyes aren't real"
+
+Выход: "How Can Mirrors Be Real If Our Eyes Aren't Real"*/
